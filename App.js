9333bd68804d85f1057092dcd16581b974ee4949
Mustafa Aleshekir <moessieweb@gmail.com>
2020-11-18 13:12:29 +0100

    Expo Eject React Native bareworkflow

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>COVID</Text>
+      <Text>CoronaCleaner</Text>
       <StatusBar style="auto" />
     </View>
   );
